0d95acc2fa27c246c40f06ab54324b43bf3dd3f2
n.shaldenkov <shaldnikita2@yandex.ru>
2018-03-26 23:36:59 +0300

    added test data

--- src/main/java/ru/shaldnikita/hadoop/spark/WordCount.java
+++ src/main/java/ru/shaldnikita/hadoop/spark/WordCount.java
@@ -1,19 +1,20 @@
 package ru.shaldnikita.hadoop.spark;
 
-/*import org.apache.spark.SparkConf;
+import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
 
 import java.util.Arrays;
-import java.util.List;*/
+import java.util.List;
+
 
 public class WordCount {
 
 
     public static void main(String[] args) {
-        /*System.setProperty("hadoop.home.dir", "D:/PROGRAMMING/winutils");
+        System.setProperty("hadoop.home.dir", "D:/PROGRAMMING/winutils");
 
         SparkConf conf = new SparkConf();
         conf.setAppName("Word counter");
@@ -21,13 +22,16 @@ public class WordCount {
 
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        JavaRDD<String> rdd = sc.textFile(*//*"file:/home/shaldnikita/Downloads/titanic.csv"*//*"inputDir");
+        JavaRDD<String> rdd = sc.textFile(
+                /*"file:/home/shaldnikita/Downloads/titanic.csv";*/
+                "inputDir");
 
         JavaPairRDD<String, Integer> counts = rdd
                 .flatMap(s -> Arrays.asList(s.split(" ")).iterator())
                 .mapToPair(word -> new Tuple2<>(word, 1))
                 .reduceByKey((a, b) -> a + b);
-        counts.saveAsTextFile("output");*/
+        counts.saveAsTextFile("output");
+
 
     }
 }
